97dfa1878eab95caa132c9f9ba632c87050f8a73
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-10-23 03:54:34 -0400

    Add: parser tryout.

--- minerva/reader.py
+++ minerva/reader.py
@@ -2,6 +2,7 @@ from HTMLParser import HTMLParser
 from urlparse import urlparse
 from urlparse import parse_qs
 from bs4 import BeautifulSoup
+from lxml import etree
 
 class MinervaReader:
 
@@ -19,6 +20,51 @@ class MinervaReader:
 		return 'Authorization Failure - you have entered an invalid McGill Username / Password.'
 
 	def transcript(self, html, semester='all'):
-		bs = BeautifulSoup(html)
+		soup = BeautifulSoup(html)
 
-		return bs.find("tr")
+		# div[4] : class="plaintable"		-> title + static header
+		# div[5] : class="staticheaders"	-> empty
+		# div[7] : class="infotextdiv"		-> Contains error message ?
+		# div[8] : class="pagefooterdiv"	-> Form Name: SWFTRAN
+		# div[9] : class="poweredbydiv"
+		# div[10]: class="div1"				-> empty
+		# div[11]: class="div2"				-> empty
+		# div[12]: class="div3"				-> empty
+		# div[13]: class="div4"				-> empty
+		# div[14]: class="div5"				-> empty
+		# div[15]: class="div6"				-> empty
+		div = soup.body.find('div', {'class':'pagebodydiv'})
+		tbl = div.find_all('table', {'class':'dataentrytable'})[-1]
+		print tbl.find_all(True)[0]
+
+		# print str(tbl.find('tr')), '<'
+		self.table2py2(tbl)
+		# soup.body.find_all('div')[-10]
+		# tables = div.find_all('table')[-3]
+		return True
+
+	def table2py2(self, fulltable):
+
+		table = fulltable.findAll('tr')
+		print table[0].text
+		rows = iter(table)
+		# headers = [col.text
+		for i, col in next(rows):print i, col.text
+		# ]
+
+		for row in rows:
+			cells = row.findAll('td')
+			values = [cell.text for cell in cells]
+			print values
+			#print dict(zip(headers, values))
+
+	def table2py(self, text):
+
+		table = etree.XML(text)
+		rows = iter(table)
+		headers = [col.text for col in next(rows)]
+		print headers
+		for row in rows:
+			values = [col.text for col in row]
+			print values
+			#print dict(zip(headers, values))
