0d5f8d11136d8c906ecf7248ce3ef98a8bda30b6
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-01-12 22:31:42 -0500

    Fix: setup.py

--- setup.py
+++ setup.py
@@ -24,13 +24,12 @@ setup(
 			'minervashadow=minervashadow:start'
 		],
 	},
-	classifiers={
-		'Development Status :: 2 - Alpha',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
 		'NaturalLanguage :: English',
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 2.6',
-	}
+		'Programming Language :: Python :: 2.7'
+	]
 )
