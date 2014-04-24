178693f63b45c9c52f71dd630605aa8c26a164c5
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-04-24 14:16:20 -0400

    Fix: pep8 ui/cli.py

--- minervashadow/__main__.py
+++ minervashadow/__main__.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 """
 Usage:
-	minervashadow login
-	minervashadow transcript
-	minervashadow -h | --help
-	minervashadow -v | --version
+    minervashadow login
+    minervashadow transcript
+    minervashadow -h | --help
+    minervashadow -v | --version
 
 Examples:
-	minervashadow login
-	minervashadow transcript
+    minervashadow login
+    minervashadow transcript
 
 Options:
-	-h --help     Show this screen.
-	-v --version  Print the current version.
+    -h --help     Show this screen.
+    -v --version  Print the current version.
 """
 
 from minervashadow import get_version
@@ -22,32 +22,31 @@ import minerva
 
 
 def interruptable(fn):
-	"""Exits the program if Ctrl+C is pressed"""
-	def wrapper(*args, **kwargs):
-		try:
-			return fn(*args, **kwargs)
-		except KeyboardInterrupt:
-			print '\n\nExiting...\nHope to see you soon!'
-			exit()
-	return wrapper
+    """Exits the program if Ctrl+C is pressed"""
+    def wrapper(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except KeyboardInterrupt:
+            print '\n\nExiting...\nHope to see you soon!'
+            exit()
+    return wrapper
 
 
 @interruptable
 def main():
 
-	args = docopt(__doc__, version=get_version())
+    args = docopt(__doc__, version=get_version())
 
-	credentials = cli.get_user_credentials()
+    credentials = cli.get_user_credentials()
 
-	session = minerva.MinervaSession(credentials)
+    session = minerva.MinervaSession(credentials)
 
-	print session.login()
+    print session.login()
 
-	if not args['login']:
-		print session.deal_with_request(args)
+    if not args['login']:
+        print session.deal_with_request(args)
 
-	session.logout()
-
+    session.logout()
 
 if __name__ == '__main__':
-	main()
+    main()
--- minervashadow/ui/cli.py
+++ minervashadow/ui/cli.py
@@ -6,144 +6,144 @@ import os
 
 def get_user_credentials():
 
-	"""Gets the user credentials from the commandline.
+    """Gets the user credentials from the commandline.
 
-	Asks the user for his McGill email address and password.
+    Asks the user for his McGill email address and password.
 
 
-	Args:
-		None
+    Args:
+        None
 
-	Returns:
-		An array containing the user credentials.
+    Returns:
+        An array containing the user credentials.
 
-		Notice that no checks for validity is applied to the input.
-		For testing purposes, if not executed in a tty, the username
-		and password are goign to be read from stdin.
+        Notice that no checks for validity is applied to the input.
+        For testing purposes, if not executed in a tty, the username
+        and password are goign to be read from stdin.
 
-		Example:
+        Example:
 
-			['username','password']
+            ['username','password']
 
-	Raises:
-		IOError: An error occurred accessing the username file.
-	"""
+    Raises:
+        IOError: An error occurred accessing the username file.
+    """
 
-	if sys.stdin.isatty():
-		# Gets the credentials from the userFile if it exists
-		_mail = getusername()
-		_pass = getpass(stream=sys.stderr)
-		_cred = [_mail, _pass]
+    if sys.stdin.isatty():
+        # Gets the credentials from the userFile if it exists
+        _mail = getusername()
+        _pass = getpass(stream=sys.stderr)
+        _cred = [_mail, _pass]
 
-	else:
-		# Gets the credentials from stdin
-		_cred = sys.stdin.readlines()
-		_cred = map(str.strip, _cred)
+    else:
+        # Gets the credentials from stdin
+        _cred = sys.stdin.readlines()
+        _cred = map(str.strip, _cred)
 
-		print 'autologin\t:', _cred[0] 
-		if len(_cred) != 2:
-			print 'Error: Malformed input. Missing arguments.'
-			print 'Here is what your input should look like:'
-			print '\tjonh.doe@mail.mcgill.ca'
-			print '\tpassword'
-			exit()
-			
-	return _cred
+        print 'autologin\t:', _cred[0]
+        if len(_cred) != 2:
+            print 'Error: Malformed input. Missing arguments.'
+            print 'Here is what your input should look like:'
+            print '\tjonh.doe@mail.mcgill.ca'
+            print '\tpassword'
+            exit()
+
+    return _cred
 
 
 def getusername(reset=False):
 
-	"""	Gets the username either from the commandline,
-		either from the user file (in ~/.minerva/).
+    """ Gets the username either from the commandline,
+        either from the user file (in ~/.minerva/).
 
-	Asks the user for his McGill email address if no previous session
-	has been stored, and save the session in the user file.
+    Asks the user for his McGill email address if no previous session
+    has been stored, and save the session in the user file.
 
 
-	Args:
-		None
+    Args:
+        None
 
-	Returns:
-		A string containing the user email.
+    Returns:
+        A string containing the user email.
 
-		Example:
+        Example:
 
-			'john.doe@mail.mcgill.ca'
+            'john.doe@mail.mcgill.ca'
 
-	Raises:
-		IOError: An error occurred accessing the username file.
-	"""
+    Raises:
+        IOError: An error occurred accessing the username file.
+    """
 
-	def _is_valid(username):
-		return ('@' and '.' in username)
+    def _is_valid(username):
+        return ('@' and '.' in username)
 
-	_mailSaved = savedusername()
-	_mail = ""
-	
-	if _is_valid(_mailSaved) and not reset:
-		print 'Read\t:', _mailSaved
-		_mail = _mailSaved
-	else:
-		_mail = raw_input('Email\t: ')
+    _mailSaved = savedusername()
+    _mail = ""
 
-		# Auto-completes the username if domain missing 
-		if '.' in _mail and not '@' in _mail:
-			_mail = _mail + '@mail.mcgill.ca'
+    if _is_valid(_mailSaved) and not reset:
+        print 'Read\t:', _mailSaved
+        _mail = _mailSaved
+    else:
+        _mail = raw_input('Email\t: ')
 
-		if not _is_valid(_mail):
-			raise InvalidUsername(_mail)
+        # Auto-completes the username if domain missing
+        if '.' in _mail and '@' not in _mail:
+            _mail = _mail + '@mail.mcgill.ca'
 
-	# Stores the email to the user file
-	if _mail != _mailSaved:
-		savedusername(_mail)
+        if not _is_valid(_mail):
+            raise InvalidUsername(_mail)
 
-	return _mail
+    # Stores the email to the user file
+    if _mail != _mailSaved:
+        savedusername(_mail)
+
+    return _mail
 
 
 def savedusername(username=""):
 
-	"""
-	Reads the email address stored in the userfile.
-	if the file does not exist, it creates it.
+    """
+    Reads the email address stored in the userfile.
+    if the file does not exist, it creates it.
 
-	It also stores the username passed as argument if it is not empty.
+    It also stores the username passed as argument if it is not empty.
 
 
-	Args:
-		A username as a string.
+    Args:
+        A username as a string.
 
-		Example:
+        Example:
 
-			'john.doe@mail.mcgill.ca'
+            'john.doe@mail.mcgill.ca'
 
-	Returns:
-		Either a string containing the username that has been read,
-		either a string containing the username that has just been stored.
+    Returns:
+        Either a string containing the username that has been read,
+        either a string containing the username that has just been stored.
 
-		Example:
+        Example:
 
-			'john.doe@mail.mcgill.ca'
+            'john.doe@mail.mcgill.ca'
 
-	Raises:
-		IOError: An error occurred accessing the username file.
-	"""
+    Raises:
+        IOError: An error occurred accessing the username file.
+    """
 
-	_userPath = os.path.expanduser("~/.minerva/")
-	_userFile = os.path.join(_userPath, 'user')
+    _userPath = os.path.expanduser("~/.minerva/")
+    _userFile = os.path.join(_userPath, 'user')
 
-	if not os.path.exists(_userPath):
-		os.makedirs(_userPath)
+    if not os.path.exists(_userPath):
+        os.makedirs(_userPath)
 
-	if not os.path.exists(_userFile):
-		open(_userFile, 'w').close()
+    if not os.path.exists(_userFile):
+        open(_userFile, 'w').close()
 
-	if username == "":
-		userFile = open(_userFile, 'r+')
-		username = userFile.readline().strip()
-		userFile.close()
-	else:
-		userFile = open(_userFile, 'w+')
-		userFile.write(username)
-		userFile.close()
+    if username == "":
+        userFile = open(_userFile, 'r+')
+        username = userFile.readline().strip()
+        userFile.close()
+    else:
+        userFile = open(_userFile, 'w+')
+        userFile.write(username)
+        userFile.close()
 
-	return username
+    return username
